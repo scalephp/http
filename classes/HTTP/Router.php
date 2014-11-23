25d744e66aa2f9e67369cf71dfbe26f718510a8a
cyrrill <cyrrill@users.noreply.github.com>
2014-11-23 17:25:46 +0100

    Router class

--- classes/HTTP/Router.php
+++ classes/HTTP/Router.php
@@ -1,44 +1,67 @@
 <?php namespace Scale\Http\HTTP;
 
+use Closure;
+use Scale\Kernel\Interfaces\ExecutorInterface;
+use Scale\Kernel\Interfaces\BuilderInterface;
+use Scale\Kernel\Core\Builders;
 use Scale\Kernel\Core\RuntimeException;
+use Scale\Http\HTTP\IO\RequestInterface;
 
-class Router
+class Router implements ExecutorInterface, BuilderInterface
 {
-    public function route()
+    use Builders;
+
+    public function __construct(
+        $uri = true,
+        $params = true,
+        RequestInterface $request = null,
+        Closure $controller = null
+    ) {
+        $this->uri = $uri;
+        $this->params = $params;
+        $this->request = $request;
+        $this->controller = $controller;
+    }
+
+    public function prepare()
     {
-        $method = $_SERVER['REQUEST_METHOD'];
+        // Autodetect task name?
+        $this->uri = ($this->uri === true) ? $this->request->uri() : $this->uri;
 
-        if ($method == 'POST') {
-            $data = $_POST;
-        } elseif ($method == 'GET') {
-            $data = $_GET;
+        // Get command line params
+        $this->params = ($this->params === true) ? $this->request->params() : $this->params;
+
+        $this->route = $this->getRoute($this->uri, $this->params);
+
+        // Create a new instance of the task
+        $this->controller = $this->controller($this->route['controller']);
+        
+        return $this;
+    }
+
+    public function getRoute($uri, $params)
+    {
+        $routes = require \App\PATH.'/etc/routes.php';
+
+        if (isset($routes[$uri])) {
+            $route = $routes[$uri];
         } else {
-            throw new RuntimeException('Invalid Request');
-        }
-
-        $route = null;
-        // Load the configured routes
-        $routes = require App\PATH.'/config/routes.php';
-
-        if (isset($data['location'])) {
-            if (isset($routes[$data['location']])) {
-                $route = $routes[$data['location']];
-            }
-        }
-
-        if (!$route) {
-
-            throw new Exception('404');
+            throw new RuntimeException('404');
         }
 
         $input = array();
 
         foreach ($route['params'] as $param) {
-            $input[$param] = isset($data[$param]) ? $data[$param] : null;
+            $input[$param] = isset($params[$param]) ? $params[$param] : null;
         }
 
-        $route['params'] = $input;
+        $route['input'] = $input;
 
         return $route;
+    }
+
+    public function execute()
+    {
+        $this->controller->{$this->route['action']}($this->route['input']);
     }
 }
